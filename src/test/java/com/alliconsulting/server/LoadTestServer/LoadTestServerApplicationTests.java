40cb31596ffb38209854abb9990014a6e9f8deed
Asif <alli.asif@gmail.com>
2020-09-05 16:55:30 -0400

    Initial commit

---
+++ src/test/java/com/alliconsulting/server/LoadTestServer/LoadTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alliconsulting.server.LoadTestServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadTestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
